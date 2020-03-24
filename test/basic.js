59ae57a3c3d191ea935e15e087cc550d1fdd7dd4
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2020-03-23 17:32:01 -0700

    added get, validate tests

--- test/basic.js
+++ test/basic.js
@@ -5,13 +5,39 @@ const lib = require('../lib/index.js');
 
 const expect = chai.expect;
 
-describe('basic', () => {
-
+describe('availability', () => {
   'header verilogBBX validate scafolder get'.split(' ').map(name => {
     it(name + ' is function', done => {
       expect(lib[name]).to.be.a('function');
       done();
     });
+  });
+});
+
+describe('get', () => {
+
+  it('root', async () => {
+    const p = lib.get({
+      value: '.',
+      filename: 'test/comp1.json5'
+    });
+    expect(p).to.be.a('Promise');
+    const res = await p;
+    expect(res).to.be.a('undefined');
+  });
+
+});
+
+describe('validate', () => {
+
+  it('component', async () => {
+    const p = lib.validate({
+      value: '.',
+      filename: 'test/comp1.json5'
+    });
+    expect(p).to.be.a('Promise');
+    const res = await p;
+    expect(res).to.be.a('undefined');
   });
 
 });
